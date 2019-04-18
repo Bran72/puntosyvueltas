7a85c58a2c9d76e52fe0c29666dcc8d755a0a166
Brandon Leininger <brandon.leininger@etudiant.univ-reims.fr>
2019-04-18 22:50:59 +0200

    Last update with some awesome features

--- src/Migrations/Version20190418203937.php
+++ src/Migrations/Version20190418203937.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190206081923 extends AbstractMigration
+final class Version20190418203937 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,8 @@ final class Version20190206081923 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, gamme_id INT DEFAULT NULL, titre VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, gamme VARCHAR(255) NOT NULL, photos LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', prix INT NOT NULL, fil VARCHAR(255) NOT NULL, surmesure TINYINT(1) NOT NULL, INDEX IDX_D34A04ADD2FD85F1 (gamme_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, gamme_id INT DEFAULT NULL, titre VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, prix INT DEFAULT NULL, fil VARCHAR(255) DEFAULT NULL, surmesure TINYINT(1) DEFAULT NULL, photos JSON DEFAULT NULL, dimensions VARCHAR(255) DEFAULT NULL, duree VARCHAR(255) DEFAULT NULL, taille_fils VARCHAR(255) DEFAULT NULL, date DATETIME NOT NULL, INDEX IDX_D34A04ADD2FD85F1 (gamme_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE gamme (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04ADD2FD85F1 FOREIGN KEY (gamme_id) REFERENCES gamme (id)');
     }
@@ -34,6 +35,7 @@ final class Version20190206081923 extends AbstractMigration
 
         $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04ADD2FD85F1');
         $this->addSql('DROP TABLE product');
+        $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE gamme');
     }
 }
